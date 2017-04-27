446807f45c4d05213ef5773c072ca25be62d549a
lyt <664009360@qq.com>
2017-04-27 18:09:39 +0800

    update sql

--- api/five.sql
+++ api/five.sql
@@ -16,6 +16,87 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL COMMENT '分类名称',
+  `description` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '分类描述',
+  `logo` varchar(256) COLLATE utf8_bin DEFAULT NULL COMMENT '分类logo',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='分类表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `choice_auth`
+--
+
+DROP TABLE IF EXISTS `choice_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choice_auth` (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `create_time` bigint(13) NOT NULL,
+  `user_nick_name` varchar(128) CHARACTER SET utf8 DEFAULT NULL COMMENT '作者昵称',
+  `user_avatar` varchar(256) CHARACTER SET utf8 DEFAULT NULL COMMENT '作者头像',
+  `user_introduction` varchar(120) CHARACTER SET utf8 DEFAULT NULL COMMENT '作者简介',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='精选作者';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choice_auth`
+--
+
+LOCK TABLES `choice_auth` WRITE;
+/*!40000 ALTER TABLE `choice_auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choice_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `choice_production`
+--
+
+DROP TABLE IF EXISTS `choice_production`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choice_production` (
+  `id` bigint(100) NOT NULL,
+  `production_id` bigint(100) NOT NULL COMMENT '作品id',
+  `create_time` bigint(13) NOT NULL COMMENT '精选时间',
+  `category_name` varchar(45) COLLATE utf8_bin NOT NULL COMMENT '精选作品所属分类',
+  `title` varchar(200) COLLATE utf8_bin NOT NULL COMMENT '精选作品标题',
+  `production_play_time` bigint(13) DEFAULT NULL COMMENT '精选作品（视频）时长',
+  `category_id` bigint(10) NOT NULL,
+  `production_cover` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '作品封面',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='精选作品';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choice_production`
+--
+
+LOCK TABLES `choice_production` WRITE;
+/*!40000 ALTER TABLE `choice_production` DISABLE KEYS */;
+/*!40000 ALTER TABLE `choice_production` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comment`
 --
 
@@ -23,7 +104,7 @@ DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comment` (
-  `id` bigint(100) NOT NULL AUTO_INCREMENT,
+  `id` bigint(100) NOT NULL,
   `user_id` bigint(100) NOT NULL,
   `mateiial_id` bigint(100) NOT NULL COMMENT '评论的视频/图文/音频等id',
   `content` varchar(300) CHARACTER SET latin1 NOT NULL COMMENT '评论内容',
@@ -42,6 +123,36 @@ CREATE TABLE `comment` (
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorite`
+--
+
+DROP TABLE IF EXISTS `favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorite` (
+  `id` bigint(20) NOT NULL,
+  `production_id` bigint(100) NOT NULL,
+  `create_time` bigint(13) DEFAULT NULL,
+  `category_name` varchar(45) CHARACTER SET utf8 NOT NULL,
+  `title` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `production_play_time` bigint(13) DEFAULT NULL,
+  `category_id` bigint(10) NOT NULL,
+  `production_cover` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '作品封面',
+  `user_id` bigint(20) NOT NULL COMMENT '收藏的用户id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='收藏表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorite`
+--
+
+LOCK TABLES `favorite` WRITE;
+/*!40000 ALTER TABLE `favorite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favorite` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -76,6 +187,60 @@ INSERT INTO `files` VALUES (6,'uploads/10000002/','02403ca82b2802edda68e7bcda487
 UNLOCK TABLES;
 
 --
+-- Table structure for table `focus_auth`
+--
+
+DROP TABLE IF EXISTS `focus_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `focus_auth` (
+  `id` bigint(100) NOT NULL,
+  `user_id` bigint(20) NOT NULL COMMENT '关注者id',
+  `auth_nick_name` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT '被关注作者昵称',
+  `auth_avatar` varchar(256) COLLATE utf8_bin DEFAULT NULL COMMENT '被关注作者头像',
+  `auth_introduction` varchar(120) COLLATE utf8_bin DEFAULT NULL COMMENT '被关注作者简介',
+  `auth_id` bigint(20) NOT NULL COMMENT '被关注作者id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='关注的作者';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `focus_auth`
+--
+
+LOCK TABLES `focus_auth` WRITE;
+/*!40000 ALTER TABLE `focus_auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `focus_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `focus_category`
+--
+
+DROP TABLE IF EXISTS `focus_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `focus_category` (
+  `id` bigint(100) NOT NULL,
+  `category_id` bigint(10) NOT NULL,
+  `category_name` varchar(45) COLLATE utf8_bin NOT NULL COMMENT '分类名称',
+  `user_id` bigint(20) NOT NULL COMMENT '关注者id',
+  `category_logo` varchar(256) COLLATE utf8_bin DEFAULT NULL COMMENT '分类logo',
+  `category_description` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '分类描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='关注的分类';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `focus_category`
+--
+
+LOCK TABLES `focus_category` WRITE;
+/*!40000 ALTER TABLE `focus_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `focus_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `production`
 --
 
@@ -95,10 +260,14 @@ CREATE TABLE `production` (
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态：已发布，未发布',
   `description` varchar(400) CHARACTER SET latin1 DEFAULT NULL COMMENT '描述',
   `content` text CHARACTER SET latin1 COMMENT '内容',
-  `category_id` bigint(100) NOT NULL DEFAULT '10000000' COMMENT '分类id，默认为10000000表示其他',
+  `category_id` bigint(10) NOT NULL DEFAULT '10000000' COMMENT '分类id，默认为10000000表示其他',
   `category_name` varchar(45) COLLATE utf8_bin NOT NULL DEFAULT '其他' COMMENT '分类名称 - 默认为‘其他’',
   `owner` tinyint(4) NOT NULL DEFAULT '0' COMMENT '权限 - 默认为0表示公开，1表示私密，2表示关注用户可见',
   `verify` tinyint(4) NOT NULL DEFAULT '0' COMMENT '审核状态，默认为0，表示未审核，1表示审核通过，2表示审核未通过',
+  `play_time` bigint(13) DEFAULT NULL COMMENT '视频时长',
+  `share_times` int(100) NOT NULL DEFAULT '0' COMMENT '被分享的次数',
+  `read_times` int(100) NOT NULL DEFAULT '0' COMMENT '阅读/观看次数',
+  `cover` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '作品封面',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='作品表';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -160,6 +329,7 @@ CREATE TABLE `users` (
   `birthday` bigint(13) DEFAULT NULL COMMENT '生日',
   `introduction` varchar(120) COLLATE utf8_bin DEFAULT NULL COMMENT '个人简介',
   `role` tinyint(4) NOT NULL DEFAULT '0' COMMENT '用户角色，默认为0表示普通用户，1表示超级管理员，2表示认证个人，3表示认证企业',
+  `share_times` int(100) NOT NULL DEFAULT '0' COMMENT '被分享次数，包括分享该作者或者其作品',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户信息表';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -170,7 +340,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (10000000,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,0),(10000001,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,0);
+INSERT INTO `users` VALUES (10000000,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,0,0),(10000001,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,0,0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -183,4 +353,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-25 17:31:18
+-- Dump completed on 2017-04-27 18:05:19
